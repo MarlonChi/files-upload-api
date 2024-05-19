f0f028c60d894a85ad745dc7b17ddbf98c148890
MarlonChi <marlonchiodelli@hotmail.com>
2024-05-18 21:51:09 -0300

    feat: add s3 connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-
-require("dotenv").config();
 
 const uri = process.env.MONGO_URL;
 
